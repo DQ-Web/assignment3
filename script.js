a14ce27e87624d3172e72495eb658453519d506f
DavidUllauri <ullauridavid@gmail.com>
2021-06-14 16:46:26 -0400

    Add columns

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ let itemNo = 1;
 
 function boardCols(col) {
     board.style = `grid-template-columns: repeat(${col}, minmax(auto, 70px))`;
-  }
+}
+
+function addCol() {
+    let modifier = 0;
+    for (let i = 1; i <= rowNo; i++) {
+        let div = document.createElement("div");
+        div.className = 'grid-item';
+        div.innerText = ++itemNo;
+        board.insertBefore(div, board.children[colNo * i + modifier]);
+        modifier++;
+    }
+    colNo++;
+    boardCols(colNo)
+}
+
+document.getElementById("addCBtn").onclick = addCol;
