41198c8f7a318c161c78d2f2a9c6ef03eccbf65b
DavidUllauri <ullauridavid@gmail.com>
2021-06-14 16:41:58 -0400

    Change the number of cols

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const board = document.getElementById("board");
 let colNo = 1;
 let rowNo = 1;
 let itemNo = 1;
+
+function boardCols(col) {
+    board.style = `grid-template-columns: repeat(${col}, minmax(auto, 70px))`;
+  }
